5ce7deda322bf9fe482f8d59fa6b3de8f90a207e
Makajeez <buhari.alhassan0@gmail.com>
2025-05-29 18:55:39 +0100

    adding

--- scripts.js
+++ scripts.js
@@ -1,1 +1,10 @@
-/* styles.css */
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+con
+
+if (toastTrigger) {
+const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+toastTrigger.addEventListener('click', () => {
+    toastBootstrap.show()
+})
+}
